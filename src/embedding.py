13ca271b5bf6f68b1d42a47c4ec251b318b40e1b
cgarciatorres7 <cgarciat96@gmail.com>
2025-03-18 21:58:40 -0500

    convert text into dict output -> embeddings.py

--- src/embedding.py
+++ src/embedding.py
@@ -7,16 +7,16 @@ from typing import List
 import numpy as np
 
 
-def download_audio(url: str)-> str:
+def download_audio(url: str)-> dict:
     yt = YouTube(url, on_progress_callback=on_progress)
     print(yt.title)
 
     ys = yt.streams.get_audio_only()
     ys.download(directories.raw)
 
-    return yt.title
+    return {"video_name" : yt.title, "id" : yt.video_id}
 
-def clip_audio_file(audio:str, time:int = 20):
+def clip_audio_file(audio:str, id:str, time:int = 20)-> List[dict]:
     seconds = time * 1000
     audio = AudioSegment.from_file(directories.raw / str(audio + ".m4a"), "m4a")
 
@@ -24,23 +24,30 @@ def clip_audio_file(audio:str, time:int = 20):
     files = []
     for i in range(0, total_duration, seconds):
         file_handle = audio[i : i + seconds].export(directories.processed / f'audio_{i}.wav', format='wav')
-        files.append(file_handle.name)
+        files.append({
+            "video_id" : id,
+            "start_time" : i / 1000,
+            "end_time" : (i + seconds) / 1000,
+            "file_path" : file_handle.name,
+            "url" : f"https://www.youtube.com/watch?v={id}&t={i/1000}"
+
+        })
 
     return files
 
-def transcribe_audio(clips: List[str])-> List[str]:
+def transcribe_audio(clips: List[dict])-> List[dict]:
     model = whisper.load_model("base")
     texts = []
 
     for clip in clips:
-        text = model.transcribe(clip)["text"]
-        texts.append(text)
-    return texts
+        text = model.transcribe(clip["file_path"])["text"]
+        clip["text"] = text
+    return clips
 
 
 if __name__ == "__main__":
-    audio = download_audio("https://www.youtube.com/watch?v=IELMSD2kdmk")
-    clips = clip_audio_file(audio)
+    audio_dict = download_audio("https://www.youtube.com/watch?v=IELMSD2kdmk")
+    clips = clip_audio_file(audio_dict["video_name"], audio_dict["id"])
     transcriptions = transcribe_audio(clips)
 
     for idx, text in enumerate(transcriptions):
