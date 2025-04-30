9f148b64bce968429ff438427dcffdcee9ab164b
cgarcia <cgarciat96@gmail.com>
2025-04-29 22:27:10 -0500

    [ADD] finishing retriever.py

--- pages/ask_questions.py
+++ pages/ask_questions.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from src.retriever import query_pinecone
 
 
 st.header("Youtube video title")
@@ -7,6 +7,16 @@ st.header("Youtube video title")
 if "video_title" in st.session_state:
     st.header(st.session_state.video_title)
     st.text_input("Ask a question about the video", key="name2")
+    
+    if st.button("Ask a question"):
+        xc = query_pinecone(st.session_state.video_url)
+        transcriptions = get_transcriptions(xc)
+        audio_time = get_audio_time(xc)
+        audio_url = get_audio_url(xc)
+        st.write(transcriptions)
+        st.write(audio_time)
+        st.write(audio_url)
+        
 else:
     st.warning("Please enter a YouTube URL in the search page first")
     
--- src/retriever.py
+++ src/retriever.py
@@ -1,16 +1,49 @@
 
-query = "which city has the largest population in the world?"
 
-# create the query vector
-xq = pc.inference.embed(
-    model=model_name,
-    inputs=[query],
-    parameters={
-        "input_type": "query",
-        "truncate": "END"
-    }
-).data[0]['values']
+import os
+from pinecone import Pinecone
+from sentence_transformers import SentenceTransformer
+import torch
 
-# now query
-xc = index.query(vector=xq, top_k=5, include_metadata=True)
-xc
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+retriever = SentenceTransformer(
+    'flax-sentence-embeddings/all_datasets_v3_mpnet-base'
+)
+retriever.to(device)
+
+# initialize connection to pinecone (get API key at app.pinecone.io)
+api_key = os.environ.get('PINECONE_API_KEY') or 'PINECONE_API_KEY'
+
+# configure client
+pc = Pinecone(api_key=api_key)
+
+
+
+def query_pinecone(query: str):
+    # configure index
+    index_name = "audio-embeddings"
+    index = pc.Index(index_name)
+
+    xq = retriever.encode([query]).tolist()
+    # now query
+    xc = index.query(vector=xq, top_k=5, include_metadata=True)
+    return xc
+
+def get_transcriptions(xc: dict):
+    return xc["matches"]
+
+def get_audio_url(xc: dict):
+    return xc["matches"][0]["metadata"]["audio_url"]
+
+def get_audio_id(xc: dict):
+    return xc["matches"][0]["metadata"]["audio_id"]
+
+def get_audio_time(xc: dict):
+    return xc["matches"][0]["metadata"]["audio_time"]
+
+def get_audio_transcription(xc: dict):
+    return xc["matches"][0]["metadata"]["audio_transcription"]
+
+def get_audio_transcription_time(xc: dict):
+    return xc["matches"][0]["metadata"]["audio_transcription_time"]
